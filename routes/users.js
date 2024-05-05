dbfe2cb87756b6deb9866cc5339c9afca23ac4ca
Cole Caccamise <colecaccamise@gmail.com>
2024-05-05 15:31:42 -0400

    added put and delete routes for users, fixed post

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,55 @@ const users = [
 	{ id: 4, name: 'Elon Musk' },
 ];
 
+router.use(express.json()); // not including this resulted in an undefined req.body
+
 router.get('/', (req, res) => {
 	res.json(users);
 });
+
+router
+	.route('/:id')
+	.get((req, res) => {
+		const id = parseInt(req.params.id);
+		const user = users.find((user) => user.id == parseInt(req.params.id));
+		if (!user) {
+			res.status(404).json({ error: `No user was found with ID ${id}` });
+			return;
+		}
+		res.json(user);
+	})
+	.post((req, res) => {
+		const id = parseInt(req.params.id);
+		const includesUser = users.some((user) => user.id == id);
+		if (includesUser) {
+			res.status(409).json({ error: 'A user already exists with ID ${id}' });
+			return;
+		}
+		users.push({ id: id, name: req.body.name });
+		res.json(users);
+	})
+	.put((req, res) => {
+		const id = parseInt(req.params.id);
+		const user = users.find((user) => user.id == id);
+		if (!user) {
+			res.status(404).json({ error: `No user was found with ID ${id}` });
+			return;
+		}
+		user.name = req.body.name;
+		res.json(users);
+	})
+	.delete((req, res) => {
+		const id = parseInt(req.params.id);
+		const user = users.find((user) => user.id == id);
+		if (!user) {
+			res
+				.status(404)
+				.json({ error: `Cannot delete. No user was found with ID ${id}` });
+			return;
+		}
+		const indexToDelete = users.findIndex((u) => u.id == user.id);
+		users.splice(indexToDelete, 1);
+		res.json(users);
+	});
 
 module.exports = router;
