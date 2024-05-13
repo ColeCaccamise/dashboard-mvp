704d3d7402cdcbafe281eb33e035132a9555dba5
Cole Caccamise <colecaccamise@gmail.com>
2024-05-12 22:02:05 -0400

    lots of work - shelling out backend API and connecting to mongodb

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,27 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-const getMessage = async () => {
-	try {
-		const response = await axios.get('/api/v1');
-		const data = response.data;
-		return data; // Return the JSON object directly
-	} catch (error) {
-		console.error('Error fetching data:', error);
-		return null; // Return null or handle the error as needed
-	}
-};
-
 function App() {
-	const fetchData = async () => {
-		const data = await getMessage();
-		if (data) {
-			return data.message;
-		}
-	};
-
-	const [message, setMessage] = useState(''); // Call the fetchData function to retrieve data
+	const [users, setUsers] = useState([]);
 
 	useEffect(() => {
-		const fetchDataAndSetMessage = async () => {
-			const data = await fetchData();
-			setMessage(data);
-		};
-
-		fetchDataAndSetMessage();
+		axios
+			.get('/api/v1/users')
+			.then((res) => {
+				const users = res.data.map((user, index) => {
+					return <li key={index}>{user.name}</li>;
+				});
+				setUsers(users);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	}, []);
 	return (
 		<div>
 			<h1>Welcome to Dashboard MVP</h1>
-			<p>{message}</p>
+			<h2>Users:</h2>
+			<ul>{users}</ul>
 		</div>
 	);
 }
