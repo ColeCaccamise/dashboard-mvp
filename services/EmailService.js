d2a69879d115bd13c9c908cdf2e6d52be0f16177
Cole Caccamise <colecaccamise@gmail.com>
2024-06-02 22:23:40 -0400

    add emails

--- services/EmailService.js
+++ services/EmailService.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import React from 'react';
 import { Resend } from 'resend';
+import { getImageByPublicId } from './ImageService.js';
+import { confirmEmailTemplate } from './EmailTemplateService.js';
 
 // import Confirm from '../client/src/emails/Confirm';
 dotenv.config({ path: '../config/config.env' });
@@ -10,14 +12,31 @@ const RESEND_API_KEY = process.env.RESEND_API_KEY;
 export const sendEmail = async (req, res, next) => {
 	const resend = new Resend(RESEND_API_KEY);
 
-	const sender = 'onboarding@resend.dev';
-	const recepient = 'colecaccamise@gmail.com';
+	const { sender, recipient, subject } = req;
+
+	console.log(sender, recipient, subject);
+
+	const logo = await getImageByPublicId('dashboard-mvp-logo-dark').then(
+		(resp) => resp
+	);
+
+	const logoUrl = logo.url;
+
+	const html = confirmEmailTemplate(
+		'Cole',
+		recipient,
+		logoUrl,
+		'ABC123',
+		'http://localhost:3000/confirm/ABC123'
+	);
+
+	console.log(logoUrl);
 
 	const emailSent = await resend.emails.send({
 		from: sender,
-		to: recepient,
-		subject: 'hello world',
-		html: '<h1>hello world</h1>',
+		to: recipient,
+		subject: subject,
+		html: html,
 	});
 
 	console.log('email sent: ', emailSent);
